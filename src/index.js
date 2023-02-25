fb0b8d4f666e702ee9601388a61dfc2fd2bcdc8d
Siarhei Tsihovich <siarhei.tsihovich@gmail.com>
2023-02-25 11:07:48 +0400

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reversNum = Math.abs(n).toString();
+    let newArr = Array.from(reversNum);
+    newArr = newArr.reverse();
+
+    if (newArr[0] == '0') {
+        newArr.splice(0, 1);
+        newArr = Number(newArr.join(""))
+    } else {
+        newArr = Number(newArr.join(""))
+    }
+    return newArr
 }
